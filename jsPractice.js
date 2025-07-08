a74ea866cc7184c7cf6a2b0d3e51807876693702
Zena Wu <organictissuemaker@users.noreply.github.com>
2025-07-07 22:43:52 -0700

    total error debug

--- jsPractice.js
+++ jsPractice.js
@@ -4,26 +4,26 @@ const shopping = {
   matcha: [8.00, 10], 
   brick: [1.00, 100],
   node: [4.00, 100]
-};
+}; 
 
-const cart = [
+const cart = [ 
   ["boba", 2], 
-  ["mochi", 6], 
-  ["matcha", 0], 
+  ["mochi", 6],  
+  ["matcha", 0],  
   ["brick", 1], 
-  ["node", 1]
-];
+  ["node", 1] 
+]; 
 
-function myCartTotal(cart) {
-  let total = 0.0;
+function myCartTotal(cart) { 
+  let total = 0.0; 
   for (let item of cart) {  //item is a tuple w/ quantity
     if (itemExists(item)) {  //check if the item exists
-     const name = item[0];
-     const quantity = item[1];
-     total += addingTotal(total, name, quantity);
-    };
-  };
-  return total; //return total at the end
+     const name = item[0]; 
+     const quantity = item[1]; 
+     total += addingTotal(name, quantity); 
+    }; 
+  }; 
+  return total; //return total at the end 
 };
 
 function itemExists(item) {
@@ -33,12 +33,12 @@ function itemExists(item) {
 function zeroStock(item){  //check if stock is now zero
   if (shopping[item][1] === 0){ //does something if true - remove from array and print the item name
     delete shopping[item];
-    console.log(item + " is out of stock") 
+    console.log(item + " is out of stock");
   };
 };
 
-function addingTotal(curr, name, quantity){
-  after = curr
+function addingTotal(name, quantity){
+  after = 0;
   while (quantity != 0 && shopping[name][1] > 0) {
     after += shopping[name][0];
     quantity -= 1;
